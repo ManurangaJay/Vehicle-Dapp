0f2df7a7f23f4e27dbfbba455204ff99075e96ca
ManurangaJay <manurangajayawardhana@gmail.com>
2024-10-27 07:27:09 +0530

    Spring boot project was created

---
+++ VehicleDapp/src/test/java/com/example/VehicleDapp/VehicleDappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.VehicleDapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VehicleDappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
